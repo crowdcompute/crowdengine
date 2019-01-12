d301da10c417f6b515be4b1180d3d010e37762c5
antreasL <ant89@live.com>
2019-01-12 12:15:57 +0000

    Add test for  SliceExists

--- common/hexutil/hexutil_test.go
+++ common/hexutil/hexutil_test.go
@@ -1,14 +1,30 @@
+// Copyright 2018 The crowdcompute:crowdengine Authors
+// This file is part of the crowdcompute:crowdengine library.
+//
+// The crowdcompute:crowdengine library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The crowdcompute:crowdengine library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the crowdcompute:crowdengine library. If not, see <http://www.gnu.org/licenses/>.
+
 package hexutil
 
 import "testing"
 
-type marshalTest struct {
-	input interface{}
-	want  string
+type tester struct {
+	input  interface{}
+	expect string
 }
 
 var (
-	encodeBytesToHexTests = []marshalTest{
+	encodeBytesToHexTests = []tester{
 		{[]byte{}, "0x"},
 		{[]byte{0}, "0x00"},
 		{[]byte{0, 'a'}, "0x0061"},
@@ -18,8 +34,8 @@ var (
 func TestEncode(t *testing.T) {
 	for _, test := range encodeBytesToHexTests {
 		enc := Encode(test.input.([]byte))
-		if enc != test.want {
-			t.Errorf("input %x: wrong encoding %s", test.input, enc)
+		if enc != test.expect {
+			t.Errorf("Input %x: Wrong hex encoding %s", test.input, enc)
 		}
 	}
 }
--- common/slices.go
+++ common/slices.go
@@ -24,7 +24,7 @@ func SliceExists(slice interface{}, item interface{}) bool {
 	s := reflect.ValueOf(slice)
 
 	if s.Kind() != reflect.Slice {
-		panic("SliceExists() given a non-slice type")
+		panic("SliceExists: given a non-slice type")
 	}
 
 	for i := 0; i < s.Len(); i++ {
