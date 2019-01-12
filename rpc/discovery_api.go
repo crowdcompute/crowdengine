0e9c7830a28308c6c1aafad8611890d49a7db67c
iNDicat0r <hosseini.mobin@gmail.com>
2019-01-12 21:30:14 +0200

    Fix fmt

--- rpc/discovery_api.go
+++ rpc/discovery_api.go
@@ -42,9 +42,9 @@ func (api *DiscoveryAPI) Discover(ctx context.Context, numberOfNodes int) ([]str
 	}
 
 	pid := peer.IDB58Encode(api.host.P2PHost.ID())
-	fmt.Println(pid)
+	log.Println(pid)
 	pid2 := api.host.P2PHost.ID().Pretty()
-	fmt.Println(pid2)
+	log.Println(pid2)
 	initialRequest := api.host.InitNodeDiscoveryReq(numberOfNodes, pid2)
 	// This is the initial forward of this message. No neighbour sent me this message, that's why the empty receivedNeighbour
 	api.host.ForwardToNeighbours(initialRequest, "")
