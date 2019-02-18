b90bfa8cd43b4b3c1fbadb3dc685753a70eec84d
AndreasL <ant89@live.com>
2019-02-18 14:01:20 +0000

    FIx bug when i is nil program was freezing

--- database/db_helper.go
+++ database/db_helper.go
@@ -1,15 +1,12 @@
 package database
 
-import (
-	"fmt"
-)
-
 // GetImageAccountFromDB returns an ImageAccount if exists in the database
 func GetImageAccountFromDB(hash string) (*ImageAccount, error) {
 	image := &ImageAccount{}
+
 	i, err := GetDB().Model(image).Get([]byte(hash))
-	if err != nil && err != ErrNotFound {
-		return nil, fmt.Errorf("There was an error getting the image from lvldb")
+	if err != nil {
+		return nil, err
 	}
 	image = i.(*ImageAccount)
 	return image, nil
@@ -19,9 +16,7 @@ func GetImageAccountFromDB(hash string) (*ImageAccount, error) {
 func GetImageFromDB(imgHash string) (*ImageLvlDB, error) {
 	image := &ImageLvlDB{}
 	i, err := GetDB().Model(image).Get([]byte(imgHash))
-	if err != nil && err != ErrNotFound {
-		return nil, fmt.Errorf("There was an error getting the image from lvldb")
-	} else if err == ErrNotFound || i == nil {
+	if err != nil {
 		return nil, err
 	}
 	image = i.(*ImageLvlDB)
